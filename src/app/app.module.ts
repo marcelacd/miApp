2044125a420827637dc3f547478a8e12dbe754dc
Marcela Carmona <marcelacarmona207@gmail.com>
2022-10-27 19:01:38 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EjemploComponent } from './components/ejemplo/ejemplo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EjemploComponent
   ],
   imports: [
     BrowserModule
